c32259a55775d7bfa1bded7d0270cbfffdc202b3
lostan89 <skeeperenator@gmail.com>
2025-11-09 16:35:50 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,18 +1,6 @@
-def create_commendation(child, lesson):
-    import random
+from datacenter import models
 
-    try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=child)
-    except Schoolkid.DoesNotExist:
-        return "Такого ученика не существует"
-    except Schoolkid.MultipleObjectsReturned:
-        return "По вашему запросу существует более одного ученика"
-    one_random_lesson = (
-        Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=lesson)
-        .order_by("?")
-        .first()
-    )
-    commendation_list = [
+COMMENDATIONS = [
         "Молодец!",
         "Отлично!",
         "Хорошо!",
@@ -22,27 +10,47 @@ def create_commendation(child, lesson):
         "Прекрасно!",
         "Ты меня очень обрадовал!",
     ]
+
+def find_schoolkid(child_name):
+    try:
+        return Schoolkid.objects.get(full_name__contains=child_name)
+    except Schoolkid.DoesNotExist:
+        return "Такого ученика не существует"
+    except Schoolkid.MultipleObjectsReturned:
+        return "По вашему запросу существует более одного ученика"
+
+
+def create_commendation(child, lesson):
+    import random
+
+    one_random_lesson = (
+        Lesson.objects.filter(year_of_study=6, group_letter="А", subject__title=lesson)
+        .order_by("?")
+        .first()
+    )
+    if not one_random_lesson:
+        return "Уроков не найдено"
     Commendation.objects.create(
-        text=random.choice(commendation_list),
+        text=random.choice(COMMENDATIONS),
         created=one_random_lesson.date,
-        schoolkid=schoolkid,
+        schoolkid=find_schoolkid(child),
         subject=one_random_lesson.subject,
         teacher=one_random_lesson.teacher,
     )
 
 
-def remove_chastisements(schoolkid):
-    from datacenter import models
-
-    for bad_chastisement in schoolkid:
-        bad_chastisement.delete()
+def remove_chastisements(child):
+    schoolkid = Chastisement.objects.filter(schoolkid=find_schoolkid(child))
+    schoolkid.delete()
     return "Замечания удалены"
 
 
-def fix_marks(schoolkid):
-    from datacenter import models
-
-    for bad_point in schoolkid:
-        bad_point.points = 5
-        bad_point.save()
+def fix_marks(child):
+    schoolkid = Marks.objects.filter(points__lt=4, schoolkid=find_schoolkid(child))
+    schoolkid.update(points=5)
     return "Оценки исправлены"
+
+def main():
+    
+if __name__ == "__main__":
+    main()
